71bcff0e9e350726c0b17f86537ead35707dd09d
shad anjum <92822708+shad-pythondev123@users.noreply.github.com>
2023-12-14 16:33:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstChar(text) {
   // your code here
+	let n= text.length;
+	for(let i=0; i<n; i++){
+		if(text[i]!= " ") return text[i];
+	}
+	return " ";
 }
 
 // Do not change the code below
